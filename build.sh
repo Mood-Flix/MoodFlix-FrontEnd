0d1192f666558f4a3360da7947ce24ec2daf3ce8
Dongha <132756629+kdh0825kr@users.noreply.github.com>
2025-08-27 17:28:33 +0900

    fix: build.sh 수정
    build.sh 수정

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-cd ../
-
-# output 폴더 생성
 mkdir -p output
-
-# 대괄호를 이스케이프 처리해서 정확히 인식
-cp -R ./\[MoodFlix-FrontEnd\]/* ./output/
-
-# output 내용을 다시 원래 폴더로 복사
-cp -R ./output/* ./\[MoodFlix-FrontEnd\]/
+rsync -av --exclude='output' ./ ./output/
