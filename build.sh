0e94fc112264cff5456c30e0d2a7489a71668315
Dongha <132756629+kdh0825kr@users.noreply.github.com>
2025-08-27 17:27:47 +0900

    fix: build.sh 수정
    build.sh 수정

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ cd ../
 # output 폴더 생성
 mkdir -p output
 
-# 대괄호가 포함된 폴더를 문자열로 처리, *는 밖에서 확장
-cp -R ./[MoodFlix-FrontEnd]/* ./output/
-cp -R ./output/* ./[MoodFlix-FrontEnd]/
+# 대괄호를 이스케이프 처리해서 정확히 인식
+cp -R ./\[MoodFlix-FrontEnd\]/* ./output/
+
+# output 내용을 다시 원래 폴더로 복사
+cp -R ./output/* ./\[MoodFlix-FrontEnd\]/
